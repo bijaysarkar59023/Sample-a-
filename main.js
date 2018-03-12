f421484b3b3059900391c673948f0c706fc3ad6b
Bijay Sarkar <bijaysarkar567@gmail.com>
2018-03-12 17:07:59 +0530

    all file added

---
+++ main.js
@@ -0,0 +1,30 @@
+var countdown = document.querySelector(".countdown");
+
+// get final day in ms
+var finalDay = new Date("jan 1, 2019 13:00:00").getTime();
+
+// update every second
+
+var intvl = setInterval(function(){
+// Get today's time and date in ms
+var today = new Date().getTime();
+
+// Distance from finalDay
+var distance = finalDay - today;
+
+// Time calculation
+var days = Math.floor(distance/(1000*60*60*24));
+var hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+var munites = Math.floor((distance%(1000*60*60))/(1000*60));
+var seconds = Math.floor((distance%(1000*60))/1000);
+
+// Display Result
+countdown.innerHTML = `
+<div>${days}<span> Days</span></div>
+<div>${hours}<span> Hours</span></div>
+<div>${munites}<span> Munites</span></div>
+<div>${seconds}<span> Seconds</span></div>`;
+},1000);
+
+
+
